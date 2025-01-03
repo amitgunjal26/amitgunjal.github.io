353fd65844c83aa76c9aec699b3ee88004033700
Amit Gunjal <amitgunjal26@gmail.com>
2025-01-04 02:04:47 +0530

    Updates 2025

--- js/materialize.js
+++ js/materialize.js
@@ -21,7 +21,7 @@ if (typeof jQuery === 'undefined') {
 ; /*
   * jQuery Easing v1.4.0 - http://gsgd.co.uk/sandbox/jquery/easing/
   * Open source under the BSD License.
-  * Copyright © 2008 George McGinley Smith
+  * Copyright 2008 George McGinley Smith
   * All rights reserved.
   * https://raw.github.com/gdsmith/jquery-easing/master/LICENSE
   */
